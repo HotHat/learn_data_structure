7b256782a6a8d93d158811f9619c7646b84c1694
luyh637 <774964107@qq.com>
2019-05-21 15:50:25 +0800

    add python unit test

---
+++ python/test/TestBinomialHeap.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestBinomialHeap(unittest.TestCase):
+    def test_hello(self):
+        self.assertEqual(1, 1)
+
+    def test_world(self):
+        self.assertEqual(1, 2)
+
+
+if __name__ == '__main__':
+    unittest.main()

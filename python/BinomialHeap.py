f03594552124937ac5249e674b3a7689abf8367b
luyh637 <774964107@qq.com>
2019-05-21 16:15:01 +0800

    add more coed

--- python/BinomialHeap.py
+++ python/BinomialHeap.py
@@ -1,3 +1,32 @@
 
+class BinomialNode:
+    def __init__(self, value):
+        self.parent = None
+        self.sibling = None
+        self.child = None
+        self.degree = 0
+        self.value = value
+
+
 class BinomialHeap:
-    pass
+    def __init__(self):
+        self.root = None
+        self.mini = None
+
+    def push(self, value):
+        pass
+
+    def pop(self):
+        pass
+
+    def merge(self, other):
+        pass
+
+    def decrease(self, key):
+        pass
+
+    def print(self):
+        pass
+
+    def __union(self, left, right):
+        pass

be86ec057d0deda792af3c8d1ed4d2a07215d008
luyh637 <774964107@qq.com>
2019-06-03 15:59:08 +0800

    add graph

---
+++ python/graph.py
@@ -0,0 +1,14 @@
+
+class GraphNode:
+    def __init__(self, start):
+        self.item = start
+        self.child = []
+
+    def add_child(self, to):
+        if to not in self.child:
+            self.child.append(to)
+
+
+class Graph:
+    def add(self, start, end):
+        pass
